a4681245df306075ddf6e53eb2058618af8be972
吴文豹 <572300808@qq.com>
2017-03-15 14:53:31 +0800

    Update jquery.toast.js

--- src/jquery.toast.js
+++ src/jquery.toast.js
@@ -1,7 +1,6 @@
-/*
-* jquery-toast by rsky
-* git repository https://github.com/ccrsky/jquery-toast/blob/master/README.md
-*/
+/**
+ * jquery-toast
+ */
 ;(function($) {
     $.extend({
         toast: function(txt, opt) {
@@ -15,7 +14,7 @@
                 clearTimeout(toastEl.data('timer'))
                 toastEl.remove();
             }
-            toastEl = $('<div id="mToast" class="'+ option.cls +'" style="position: fixed;bottom: 50px;left: 30px;right: 30px;text-align: center;"><span style="display: inline-block;box-sizing: border-box;background-color: rgba(0,0,0,.5);color: #fff;padding: 5px 8px;max-width: 100%;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">' + txt + '</span></div>');
+            toastEl = $('<div id="mToast" class="'+ option.cls +'" style="position: fixed;bottom: 40%;left: 5%;right: 5%;text-align: center;z-index: 999999;pointer-events: none;"><span style="display: inline-block;box-sizing: border-box;background-color: rgba(0,0,0,.5);color: #fff;padding: 5px 8px;max-width: 100%;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">' + txt + '</span></div>');
             $('body').append(toastEl);
             toastEl.fadeIn(option.transitionTime);
             toastEl.data('timer', setTimeout(function() {
